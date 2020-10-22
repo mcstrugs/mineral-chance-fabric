f9f98b4ac533856caf5bd7d5114ad2558ead7110
Joe Martinson <martinsonjoe2@gmail.com>
2020-10-22 09:49:24 -0700

    updated properties and renamed

---
+++ src/main/java/net/testmod/TestMod.java
@@ -0,0 +1,13 @@
+package net.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestMod implements ModInitializer {
+
+    @Override
+    public void onInitialize()
+    {
+        
+    }
+    
+}
